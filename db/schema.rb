62509f92780f22ff5d5b4f20f8fb3f42f2dd4769
Ben Brinckerhoff <ben@freeagent.com>
2014-08-19 12:40:44 -0700

    Adds users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819170750) do
+ActiveRecord::Schema.define(version: 20140819194008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
